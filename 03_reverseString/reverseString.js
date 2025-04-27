a251df568f4f7b4b333097c6aa350c371d272f81
Azryl Shamin <azryl.career@gmail.com>
2025-04-27 21:22:07 +0800

    Solve exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "";
 
+    for (let i = 1; i <= string.length; i++) {
+        result += string.at(0-i);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
