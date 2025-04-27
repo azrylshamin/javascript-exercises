9f663a954804761b40e466aaeeb45c1cc387da91
Azryl Shamin <azryl.career@gmail.com>
2025-04-27 21:54:00 +0800

    Solve exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements) {
+        while (array.includes(element)){
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] === element) {
+                    array.splice(i, 1);
+                }
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
