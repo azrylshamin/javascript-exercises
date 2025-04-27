e81afb4d8f11397e9f675f1eb5505bf9abdaa75e
Azryl Shamin <azryl.career@gmail.com>
2025-04-28 01:34:47 +0800

    Solve exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+
+  if (Number.isInteger(celsius)){
+    return +celsius;
+  }
+
+  return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius * (9/5) + 32);
+
+  if (Number.isInteger(fahrenheit)) {
+     return +fahrenheit;
+  }
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
