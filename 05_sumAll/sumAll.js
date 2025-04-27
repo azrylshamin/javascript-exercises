69de27ca1182795a5da0a18033f1b15978fae6fe
Azryl Shamin <azryl.career@gmail.com>
2025-04-27 22:25:16 +0800

    Solve exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    let result = 0;
+    
+    let start = num1;
+    let end = num2;
+    
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
