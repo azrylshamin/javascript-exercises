2fab5e3205d2312a6e7e02a694f8bb759e400910
Azryl Shamin <azryl.career@gmail.com>
2025-05-05 05:55:38 +0800

    Solve exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceNumber) {
 
+    if (sequenceNumber < 0) return "OOPS";
+
+    if (sequenceNumber == 0) return 0;
+
+    if (sequenceNumber == 1) return 1;
+
+    return (fibonacci(sequenceNumber - 1) + fibonacci(sequenceNumber - 2));
 };
 
 // Do not edit below this line
