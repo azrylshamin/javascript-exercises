c73e193e8241bbb3146f963e69934ae2c0cf8704
Azryl Shamin <azryl.career@gmail.com>
2025-05-05 06:31:36 +0800

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
+    peoples.sort((a, b) => {
+        const date = new Date()
+        if (a.yearOfDeath === undefined){
+            a.yearOfDeath = date.getFullYear();
+        }
+        
+        if (b.yearOfDeath === undefined){
+            b.yearOfDeath = date.getFullYear();
+        }
 
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    });
+
+    return peoples[0];
 };
 
 // Do not edit below this line
