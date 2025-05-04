72746088984a5e36a150bc91bbd5eac14d582f38
Azryl Shamin <azryl.career@gmail.com>
2025-05-05 05:34:18 +0800

    Solve exercise 09_Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().split('').filter(char => {
+                return /[a-zA-Z0-9 ]/.test(char);
+                }).join('').split(' ').join('');
+    
+    strArr = str.split('');
+    if (str === strArr.reverse().join('')) {
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
